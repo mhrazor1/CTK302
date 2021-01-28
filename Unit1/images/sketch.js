e3618e5d9587b6efc617f065b83f05b151ffc5f4
mhrazor1 <33130720+mhrazor1@users.noreply.github.com>
2021-01-28 11:32:27 -0600

    images initial commit

---
+++ Unit1/images/sketch.js
@@ -0,0 +1,25 @@
+function preload() {
+  p1=loadImage('assets/1.jpg');
+  p2=loadImage('assets/2.jpg');
+  p3=loadImage('assets/3.jpg');
+  createCanvas(0, 0);
+}
+
+function setup() {
+  p1.resize(windowWidth,0);
+  p2.resize(windowWidth,0);
+  p3.resize(windowWidth,0);
+  imgHeight = p1.height + p2.height + p3.height;
+  resizeCanvas(windowWidth, imgHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  image(p1,width/2,p1.height/2);
+  image(p2,width/2,p1.height+(p2.height/2));
+  image(p3,width/2,p1.height+p2.height+(p3.height/2));
+}
+
+function windowResized() {
+  setup();
+}
