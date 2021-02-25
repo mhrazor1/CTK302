f67d72e7a0d6c5437c1e404c19af3c0cf3b103da
Miles Howley <mhrazor1@gmail.com>
2021-02-25 10:41:00 -0600

    touches6

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -18,22 +18,22 @@ function draw() {
       case 1:
       text("it's kind of lonely here", 5, 22);
       fill('#ff6670');
-      circle(touches[0].x,touches[0].y,50);
+      circle(touches[0].x,touches[0].y,150);
       break ;
 
       case 2:
       text("two fingers are touching the screen", 5, 22);
       fill('#ff6670');
-      circle(touches[0].x,touches[0].y,50);
-      circle(touches[1].x,touches[1].y,50);
+      circle(touches[0].x,touches[0].y,150);
+      circle(touches[1].x,touches[1].y,150);
       break ;
 
       case 3:
       text("three are touching the screen", 5, 22);
       fill('#ff6670');
-      circle(touches[0].x,touches[0].y,50);
-      circle(touches[1].x,touches[1].y,50);
-      circle(touches[2].x,touches[2].y,50);
+      circle(touches[0].x,touches[0].y,150);
+      circle(touches[1].x,touches[1].y,150);
+      circle(touches[2].x,touches[2].y,150);
       break ;
 
 
