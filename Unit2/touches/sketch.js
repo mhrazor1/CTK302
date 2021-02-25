42edc938d2e17bc0cccc5d8e3362e3f2de13ed37
Miles Howley <mhrazor1@gmail.com>
2021-02-25 10:37:11 -0600

    touches4

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -12,24 +12,27 @@ function draw() {
 
   switch(numberOfTouches) {
     case 0:
-      text("no one is touching the screen", 5, 22) ;
+      text("no one is touching the screen", 5, 22);
       break ;
 
       case 1:
-      text("it's kind of lonely here", 5, 22) ;
-      fill('#ff6670')
+      text("it's kind of lonely here", 5, 22);
+      fill('#ff6670');
       circle(touches[0].x,touches[0].y,50);
       break ;
 
       case 2:
-      text("two fingers are touching the screen", 5, 22) ;
-      fill('#ff6670')
+      text("two fingers are touching the screen", 5, 22);
+      fill('#ff6670');
+      circle(touches[0].x,touches[0].y,50);
       circle(touches[1].x,touches[1].y,50);
       break ;
 
       case 3:
-      text("three are touching the screen", 5, 22) ;
-      fill('#ff6670')
+      text("three are touching the screen", 5, 22);
+      fill('#ff6670');
+      circle(touches[0].x,touches[0].y,50);
+      circle(touches[1].x,touches[1].y,50);
       circle(touches[2].x,touches[2].y,50);
       break ;
 
